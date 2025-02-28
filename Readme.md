d839ebf04d1aa8675fe1d8bf6cbd75c56c5b4cee
mdc-2468 <mandychan1019@outlook.com>
2025-03-01 05:16:24 +0800

    Refactor to user numberMap, and switched to use capture value from the UI, instead of storing tempNum when user keep on clicking the numbers' button

--- Readme.md
+++ Readme.md
@@ -7,4 +7,12 @@ This is a calculator for a learning progress.
 ## 2025-02-28
 
 - Simple calculator that have add/ subtract/ multiply/ divide function
-- Clear and dot function isn't ready
+- Refactor to use number map with key-value pair object instead of converting the HTML id for number to number in the function
+- Switched to use the method of clicking a number will update the display; and get the display innerText for calculation
+- Fixing the dot issue, and '0' after dot cases
+
+## Known Issue
+
+- Clear function isn't ready
+- big decimal issue after 2dp
+- After equal sign, and continue with calculation doesn't work; ` 1``+``2``= `... by the time the UI should reflect `3`; it's expected to use 3 to follow thereafter calculations, however the numArr has lenght >2 by update Operator function.
--- script.js
+++ script.js
@@ -1,75 +1,163 @@
+// 'use strict';
+
+// let tempNum = 0;
+// let ans = 0;
+// let isDot = false;
+// let numArr = [];
+// let operator = '';
+// let numMap = { "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0, "dot": 'dot' };
+// let operatorsList = ["add", "minus", "multiply", "divide", "equal"];
+
+
+// const btnLength = document.querySelectorAll('.btn').length;
+// const displayValue = document.querySelector("#ansDisplay");
+
+
+// for (let i = 0; i < btnLength; i++) {
+//   document.querySelectorAll('.btn')[i].addEventListener("click", calculator);
+// }
+
+// function calculator() {
+//   console.log(`(...${this.id}...)`);
+
+//   if (operatorsList.includes(this.id) && numArr.length === 1 && tempNum != 0) {
+//     updateTempNum(this.id);
+//     calculate(numArr[0], tempNum);
+//     updateOperator(this.id);
+//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
+//     return;
+
+//   } else if (this.id in numMap) {
+//     updateTempNum(this.id);
+//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
+//     return;
+
+//   } else if (operatorsList.includes(this.id)) {
+//     updateOperator(this.id);
+//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
+//     return;
+
+//   } else if (this.id === "clearInput") {
+//     clearValues();
+//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
+//   }
+// }
+
+// function updateTempNum(btnId) {
+//   if (btnId === "dot") {
+//     isDot = true;
+//     displayValue.textContent = tempNum + '.';
+//     return;
+
+//   } else if (btnId in numMap && isDot === false) {
+//     let n = numMap[btnId];
+//     tempNum = tempNum * 10 + n
+//     displayValue.textContent = tempNum;
+//     return;
+
+//   } else if (btnId in numMap && isDot === true) {
+//     tempNum = (tempNum * 10 + numMap[btnId]) / Math.pow(10, String(tempNum).length);
+//     displayValue.textContent = tempNum;
+//   }
+// }
+
+// function updateOperator(btnId) {
+//   operator = btnId;
+//   isDot = false;
+//   if (tempNum != 0) {
+//     numArr.push(tempNum);
+//     tempNum = 0;
+//   }
+// }
+
+// function calculate(a, b) {
+//   switch (operator) {
+//     case "add": ans = a + b; break;
+//     case "minus": ans = a - b; break;
+//     case "multiply": ans = a * b; break;
+//     case "divide": ans = a / b; break;
+//   }
+//   console.log(`Ans is ${ans}`);
+//   numArr = [ans];
+//   tempNum = 0;
+//   displayValue.textContent = ans;
+// }
+
+// function clearValues() {
+//   if (tempNum != 0) {
+//     tempNum = 0;
+//     displayValue.textContent = tempNum;
+
+//   } else {
+//     numArr.pop();
+//     operator = ""
+//     displayValue.textContent = tempNum;
+//   }
+// }
+
+// // ---------------------------------------------------------------------------------
+
+
 'use strict';
 
-let tempNum = 0;
+let tempNum = '';
 let ans = 0;
+// let isDot = false;
 let numArr = [];
 let operator = '';
-let numbersList = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"];
+let numMap = { "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0, "dot": '.' };
 let operatorsList = ["add", "minus", "multiply", "divide", "equal"];
 
+const calDisplay = document.querySelector("#ansDisplay");
+const btnList = document.querySelectorAll(".btn");
 
-const btnLength = document.querySelectorAll('.btn').length;
-const displayValue = document.querySelector("#ansDisplay");
-
-
-for (let i = 0; i < btnLength; i++) {
-  document.querySelectorAll('.btn')[i].addEventListener("click", calculator);
-}
+for (let i = 0; i < btnList.length; i++) {
+  btnList[i].addEventListener("click", calculator)
+};
 
 function calculator() {
-  console.log(`(...${this.id}...)`);
-  console.log(`Begin with TempNum: ${tempNum}, numArr: ${numArr}, Operator: ${operator}`);
-  if (operator !== '' && numArr.length === 1 && tempNum != 0 && operatorsList.includes(this.id)) {
-    updateTempNum(this.id);
-    calculate(numArr[0], tempNum);
-    updateOperator(this.id);
-    return;
-  } else if (numbersList.includes(this.id)) {
-    updateTempNum(this.id);
-    return;
-  } else if (operatorsList.includes(this.id)) {
-    updateOperator(this.id);
-    return;
-  }
+  console.log(this.id);
+  if (this.id in numMap) {
+    updateCalDisplay(this.id);
 
+  } else if (operatorsList.includes(this.id)) {
+    if (numArr.length === 1 && tempNum != '') {
+      calculate(Number(numArr[0]), operator, this.id);
+    } else {
+      updateOperator(this.id)
+    };
+
+  }
+  console.log(`tempNum: ${tempNum}, numArr: String[${numArr}], operator: ${operator}`);
 }
 
-function updateTempNum(btnId) {
-  if (numbersList.includes(btnId)) {
-    let n = 1;
-    switch (btnId) {
-      case "one": n = 1; break;
-      case "two": n = 2; break;
-      case "three": n = 3; break;
-      case "four": n = 4; break;
-      case "five": n = 5; break;
-      case "six": n = 6; break;
-      case "seven": n = 7; break;
-      case "eight": n = 8; break;
-      case "nine": n = 9; break;
-      case "zero": n = 0; break;
-    }
-    tempNum = tempNum * 10 + n
-    displayValue.textContent = tempNum;
-  }
+function updateCalDisplay(btnId) {
+  tempNum += numMap[btnId];
+  calDisplay.innerText = tempNum;
 }
 
 function updateOperator(btnId) {
-  operator = btnId;
-  if (tempNum != 0) numArr.push(tempNum);
-  tempNum = 0;
+  if (btnId != "equal" || btnId != "clearInput") operator = btnId;
+  numArr.push(Number(calDisplay.innerText));
+  calDisplay.innerText = '';
+  tempNum = '';
+
 }
 
-function calculate(a, b) {
-  console.log(`Calculating...`);
+function calculate(a, operator, btnId) {
+  console.log('checking');
+  console.log(`operator: ${operator}`);
+  console.log(`btnId: ${btnId}`);
+  const b = Number(calDisplay.innerText);
   switch (operator) {
     case "add": ans = a + b; break;
     case "minus": ans = a - b; break;
     case "multiply": ans = a * b; break;
     case "divide": ans = a / b; break;
   }
-  console.log(`Ans is ${ans}`);
-  numArr = [ans];
-  tempNum = 0;
-  displayValue.textContent = ans;
+  numArr = [String(ans)];
+  console.log(numArr);
+  calDisplay.innerText = String(ans);
+  if (btnId != "equal" || btnId != "clearInput") operator = btnId;
+  tempNum = '';
 }
