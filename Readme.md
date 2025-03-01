edbd8919eae3951aa792950faf25b3e493b9cf47
mdc-2468 <mandychan1019@outlook.com>
2025-03-02 01:40:55 +0800

    Added zero-division handling, negative numbers' calculations, multiple clicks for dot and negative buttons

--- Readme.md
+++ Readme.md
@@ -6,13 +6,22 @@ This is a calculator for a learning progress.
 
 ## 2025-02-28
 
-- Simple calculator that have add/ subtract/ multiply/ divide function
-- Refactor to use number map with key-value pair object instead of converting the HTML id for number to number in the function
-- Switched to use the method of clicking a number will update the display; and get the display innerText for calculation
-- Fixing the dot issue, and '0' after dot cases
+- Implemented `add`/ `subtract`/ `multiply`/ `divide` functions
+- Refactored to use number map with key-value pair object instead of converting the HTML id to number in the function
+- Enhanced the method for capturing the input value (system will capture all the input in the innerText, and render the innerText for calculations)
+- Handled the `dot` for calculation
+- Fixed the issue for inputting 0 after a `dot` (i.e: 1.09)
 
-## Known Issue
+## 2025-03-01
 
-- Clear function isn't ready
-- big decimal issue after 2dp
-- After equal sign, and continue with calculation doesn't work; ` 1``+``2``= `... by the time the UI should reflect `3`; it's expected to use 3 to follow thereafter calculations, however the numArr has lenght >2 by update Operator function.
+- Implemented the `clear` input function to reset the input and data
+- Added handling for the error case of zero-division
+- Added handling for calculation with `negative` numbers
+- Added handling for clicking `negative` button to toggle on and off the `-` sign prefix
+- Added handling for clicking `dot` button twice that should not show up two dots in the display
+- Fixed an issue where calculations after pressing `equal` did not work correctly. (Previously, an extra value was incorrectly pushed to numArr)
+- Fix an issue where clicking the `negative` button after obtaining a result with `equal` would incorrectly prepend an extra minus sign (e.g., --12). After the change, it should return `12`
+
+# Known Issue
+
+- big decimal issue
--- script.js
+++ script.js
@@ -1,111 +1,133 @@
 // 'use strict';
 
-// let tempNum = 0;
+// let tempNum = '';
 // let ans = 0;
-// let isDot = false;
 // let numArr = [];
 // let operator = '';
-// let numMap = { "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0, "dot": 'dot' };
+// let numMap = { "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0, "dot": '.', "negative": -1 };
 // let operatorsList = ["add", "minus", "multiply", "divide", "equal"];
 
+// const calDisplay = document.querySelector("#ansDisplay");
+// const btnList = document.querySelectorAll(".btn");
 
-// const btnLength = document.querySelectorAll('.btn').length;
-// const displayValue = document.querySelector("#ansDisplay");
-
-
-// for (let i = 0; i < btnLength; i++) {
-//   document.querySelectorAll('.btn')[i].addEventListener("click", calculator);
-// }
+// for (let i = 0; i < btnList.length; i++) {
+//   btnList[i].addEventListener("click", calculator)
+// };
 
 // function calculator() {
-//   console.log(`(...${this.id}...)`);
-
-//   if (operatorsList.includes(this.id) && numArr.length === 1 && tempNum != 0) {
-//     updateTempNum(this.id);
-//     calculate(numArr[0], tempNum);
-//     updateOperator(this.id);
-//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
-//     return;
-
-//   } else if (this.id in numMap) {
-//     updateTempNum(this.id);
-//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
-//     return;
-
-//   } else if (operatorsList.includes(this.id)) {
-//     updateOperator(this.id);
-//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
-//     return;
+//   console.log(`(${this.id})`);
+//   if (this.id in numMap) {
+//     updateCalDisplay(this.id);
 
 //   } else if (this.id === "clearInput") {
-//     clearValues();
-//     console.log(`After changes: TempNum= ${tempNum}, numArr= ${numArr}, Operator= ${operator}`);
+//     clear();
+
+//   } else if (operatorsList.includes(this.id)) {
+//     if (numArr.length === 1 && tempNum != '') {
+//       calculate(Number(numArr[0]), operator, this.id);
+//       if (this.id !== 'equal') operator = this.id;
+
+//     } else {
+//       updateOperator(this.id);
+//     };
 //   }
+
+//   console.log(`After processing: tempNum= ${tempNum}, numArr= String[${numArr}], operator= ${operator}`);
 // }
 
-// function updateTempNum(btnId) {
-//   if (btnId === "dot") {
-//     isDot = true;
-//     displayValue.textContent = tempNum + '.';
+// function updateCalDisplay(btnId) {
+//   if (tempNum[tempNum.length - 1] === '.' && btnId === "dot") {
 //     return;
+//   } else if (btnId === 'negative' && tempNum === '' && numArr.length === 1) {
+//     // Handle clicking 'negative' after clicking 'equal', where tempNum is empty now, and numArr has a length of 1
+//     let lastAns = numArr[0];
+//     if (lastAns[0] === '-') {
+//       const revertNewNum = lastAns.slice(1, lastAns.length);
+//       tempNum = revertNewNum;
+//     } else {
+//       tempNum = '-' + numArr[0];
+//     }
+//     numArr.pop();
 
-//   } else if (btnId in numMap && isDot === false) {
-//     let n = numMap[btnId];
-//     tempNum = tempNum * 10 + n
-//     displayValue.textContent = tempNum;
-//     return;
+//   } else if (btnId === "negative" && tempNum[0] != '-') {
+//     // Handle clicking 'negative' after inputting some numbers and is storing in tempNum
+//     console.log(`Turning number to be negative`);
+//     tempNum = String("-") + tempNum;
+//     console.log(tempNum);
 
-//   } else if (btnId in numMap && isDot === true) {
-//     tempNum = (tempNum * 10 + numMap[btnId]) / Math.pow(10, String(tempNum).length);
-//     displayValue.textContent = tempNum;
-//   }
+//   } else if (btnId === "negative" && tempNum[0] === '-') {
+//     // Handle clicking 'negative' on an tempNum with prefix = '-' to revert the tempNum from negative value to positive value
+//     console.log(`Revering the negative`);
+//     const revertNewNum = tempNum.slice(1, tempNum.length);
+//     tempNum = revertNewNum;
+//     console.log(tempNum);
+
+
+//   } else if (btnId !== "negative") {
+//     tempNum += numMap[btnId];
+//   };
+//   calDisplay.innerText = tempNum;
 // }
 
 // function updateOperator(btnId) {
-//   operator = btnId;
-//   isDot = false;
-//   if (tempNum != 0) {
-//     numArr.push(tempNum);
-//     tempNum = 0;
+//   if (btnId !== 'equal' && btnId !== 'clearInput') {
+//     console.log(`Updating operator from ${operator} to ${btnId}`);
+//     operator = btnId;
 //   }
+//   if (tempNum != '') {
+//     numArr.push(Number(calDisplay.innerText));
+//   }
+//   calDisplay.innerText = '';
+//   tempNum = '';
+
 // }
 
-// function calculate(a, b) {
-//   switch (operator) {
-//     case "add": ans = a + b; break;
-//     case "minus": ans = a - b; break;
-//     case "multiply": ans = a * b; break;
-//     case "divide": ans = a / b; break;
+// function calculate(a, operator, btnId) {
+//   const b = Number(calDisplay.innerText);
+
+//   if (b != 0) {
+//     switch (operator) {
+//       case "add": ans = a + b; break;
+//       case "minus": ans = a - b; break;
+//       case "multiply": ans = a * b; break;
+//       case "divide": ans = a / b; break;
+//     }
+//   } else if (b == 0 && operator === 'divide') {
+//     ans = 'Err';
 //   }
-//   console.log(`Ans is ${ans}`);
-//   numArr = [ans];
-//   tempNum = 0;
-//   displayValue.textContent = ans;
+//   numArr = [String(ans)];
+//   calDisplay.innerText = String(ans);
+
+//   if (btnId !== 'equal' && btnId !== 'clearInput') {
+//     console.log('getting into the inner loop of calculate');
+//     console.log(`operator: ${operator}`);
+//     console.log(`btnId: ${btnId}`);
+//     console.log(`going to convert operator ${operator} to be ${btnId}`);
+//   };
+//   tempNum = '';
 // }
 
-// function clearValues() {
-//   if (tempNum != 0) {
-//     tempNum = 0;
-//     displayValue.textContent = tempNum;
-
+// function clear() {
+//   if (tempNum != '') {
+//     tempNum = '';
 //   } else {
 //     numArr.pop();
-//     operator = ""
-//     displayValue.textContent = tempNum;
+//     operator = '';
 //   }
+
+//   calDisplay.innerText = '';
 // }
 
-// // ---------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------
 
 
 'use strict';
 
 let tempNum = '';
 let ans = 0;
-// let isDot = false;
 let numArr = [];
 let operator = '';
-let numMap = { "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0, "dot": '.' };
+let numMap = { "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "zero": 0, "dot": '.', "negative": -1 };
 let operatorsList = ["add", "minus", "multiply", "divide", "equal"];
 
 const calDisplay = document.querySelector("#ansDisplay");
@@ -116,48 +138,105 @@ for (let i = 0; i < btnList.length; i++) {
 };
 
 function calculator() {
-  console.log(this.id);
+  console.log(`(${this.id})`);
   if (this.id in numMap) {
     updateCalDisplay(this.id);
+
+  } else if (this.id === "clearInput") {
+    clear();
 
   } else if (operatorsList.includes(this.id)) {
     if (numArr.length === 1 && tempNum != '') {
       calculate(Number(numArr[0]), operator, this.id);
-    } else {
-      updateOperator(this.id)
-    };
+      if (this.id !== 'equal') operator = this.id;
 
+    } else {
+      updateOperator(this.id);
+    };
   }
-  console.log(`tempNum: ${tempNum}, numArr: String[${numArr}], operator: ${operator}`);
+
+  console.log(`After processing: tempNum= ${tempNum}, numArr= String[${numArr}], operator= ${operator}`);
 }
 
 function updateCalDisplay(btnId) {
-  tempNum += numMap[btnId];
+  if (tempNum[tempNum.length - 1] === '.' && btnId === "dot") {
+    return;
+  } else if (btnId === 'negative' && tempNum === '' && numArr.length === 1) {
+    // Handle clicking 'negative' after clicking 'equal', where tempNum is empty now, and numArr has a length of 1
+    let lastAns = numArr[0];
+    if (lastAns[0] === '-') {
+      const revertNewNum = lastAns.slice(1, lastAns.length);
+      tempNum = revertNewNum;
+    } else {
+      tempNum = '-' + numArr[0];
+    }
+    numArr.pop();
+
+  } else if (btnId === "negative" && tempNum[0] != '-') {
+    // Handle clicking 'negative' after inputting some numbers and is storing in tempNum
+    console.log(`Turning number to be negative`);
+    tempNum = String("-") + tempNum;
+    console.log(tempNum);
+
+  } else if (btnId === "negative" && tempNum[0] === '-') {
+    // Handle clicking 'negative' on an tempNum with prefix = '-' to revert the tempNum from negative value to positive value
+    console.log(`Revering the negative`);
+    const revertNewNum = tempNum.slice(1, tempNum.length);
+    tempNum = revertNewNum;
+    console.log(tempNum);
+
+
+  } else if (btnId !== "negative") {
+    tempNum += numMap[btnId];
+  };
   calDisplay.innerText = tempNum;
 }
 
 function updateOperator(btnId) {
-  if (btnId != "equal" || btnId != "clearInput") operator = btnId;
-  numArr.push(Number(calDisplay.innerText));
+  if (btnId !== 'equal' && btnId !== 'clearInput') {
+    console.log(`Updating operator from ${operator} to ${btnId}`);
+    operator = btnId;
+  }
+  if (tempNum != '') {
+    numArr.push(Number(calDisplay.innerText));
+  }
   calDisplay.innerText = '';
   tempNum = '';
 
 }
 
 function calculate(a, operator, btnId) {
-  console.log('checking');
-  console.log(`operator: ${operator}`);
-  console.log(`btnId: ${btnId}`);
   const b = Number(calDisplay.innerText);
-  switch (operator) {
-    case "add": ans = a + b; break;
-    case "minus": ans = a - b; break;
-    case "multiply": ans = a * b; break;
-    case "divide": ans = a / b; break;
+
+  if (b != 0) {
+    switch (operator) {
+      case "add": ans = a + b; break;
+      case "minus": ans = a - b; break;
+      case "multiply": ans = a * b; break;
+      case "divide": ans = a / b; break;
+    }
+  } else if (b == 0 && operator === 'divide') {
+    ans = 'Err';
   }
   numArr = [String(ans)];
-  console.log(numArr);
   calDisplay.innerText = String(ans);
-  if (btnId != "equal" || btnId != "clearInput") operator = btnId;
+
+  if (btnId !== 'equal' && btnId !== 'clearInput') {
+    console.log('getting into the inner loop of calculate');
+    console.log(`operator: ${operator}`);
+    console.log(`btnId: ${btnId}`);
+    console.log(`going to convert operator ${operator} to be ${btnId}`);
+  };
   tempNum = '';
+}
+
+function clear() {
+  if (tempNum != '') {
+    tempNum = '';
+  } else {
+    numArr.pop();
+    operator = '';
+  }
+
+  calDisplay.innerText = '';
 }
